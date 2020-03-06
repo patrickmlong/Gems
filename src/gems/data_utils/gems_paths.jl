79524c384a65a7f05e67d3ca882e1125552ccf13
Patrick Long <patrick.long@gmail.com>
2020-03-05 21:21:15 -0500

    add gems paths

---
+++ src/gems/data_utils/gems_paths.jl
@@ -0,0 +1,6 @@
+SAVE_PATH = "./data/raw_data/gems/"                                         
+URL_CMS = "https://www.cms.gov/Medicare/Coding/"
+
+URL_GEMS = string(URL_CMS, "ICD10/Downloads/2018-ICD-10-CM-General-Equivalence-Mappings.zip")
+URL_ICD10  = string(URL_CMS, "ICD10/Downloads/2018-ICD-10-Code-Descriptions.zip")
+URL_ICD9 =  string(URL_CMS, "ICD9ProviderDiagnosticCodes/Downloads/ICD-9-CM-v32-master-descriptions.zip")
