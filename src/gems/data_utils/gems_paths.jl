0caaf7e3f119ef1a367178c85138e483c4dce362
Patrick Long <patrick.long@gmail.com>
2020-03-05 21:42:23 -0500

    update cms file paths

--- src/gems/data_utils/gems_paths.jl
+++ src/gems/data_utils/gems_paths.jl
@@ -4,3 +4,7 @@ URL_CMS = "https://www.cms.gov/Medicare/Coding/"
 URL_GEMS = string(URL_CMS, "ICD10/Downloads/2018-ICD-10-CM-General-Equivalence-Mappings.zip")
 URL_ICD10  = string(URL_CMS, "ICD10/Downloads/2018-ICD-10-Code-Descriptions.zip")
 URL_ICD9 =  string(URL_CMS, "ICD9ProviderDiagnosticCodes/Downloads/ICD-9-CM-v32-master-descriptions.zip")
+
+gems_files = download(URL_GEMS, "gems.zip")
+icd10_files = download(URL_ICD10, "icd10.zip")
+icd9_files = download(URL_ICD9, "icd9.zip")
