d7d0cff95df8678fc4b076005e2df9fbbfd196f0
Patrick Long <patrick.long@gmail.com>
2020-03-05 21:49:45 -0500

    update download paths to data/

--- src/gems/data_utils/gems_paths.jl
+++ src/gems/data_utils/gems_paths.jl
@@ -5,6 +5,6 @@ URL_GEMS = string(URL_CMS, "ICD10/Downloads/2018-ICD-10-CM-General-Equivalence-M
 URL_ICD10  = string(URL_CMS, "ICD10/Downloads/2018-ICD-10-Code-Descriptions.zip")
 URL_ICD9 =  string(URL_CMS, "ICD9ProviderDiagnosticCodes/Downloads/ICD-9-CM-v32-master-descriptions.zip")
 
-gems_files = download(URL_GEMS, "gems.zip")
-icd10_files = download(URL_ICD10, "icd10.zip")
-icd9_files = download(URL_ICD9, "icd9.zip")
+gems_files = download(URL_GEMS, "../../../data/gems.zip")
+icd10_files = download(URL_ICD10, "../../../data/icd10.zip")
+icd9_files = download(URL_ICD9, "../../../data/icd9.zip")
