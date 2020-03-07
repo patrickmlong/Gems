89006cadb5ddafc0b77c24ae77c33f728f791eb3
Patrick Long <patrick.long@gmail.com>
2020-03-07 17:18:43 -0500

    add gems_utils

---
+++ src/gems/utils/gems_utils.jl
@@ -0,0 +1,46 @@
+using DataFrames
+using CSV
+using DataFramesMeta
+
+function set_map_type(df,
+        flag_type:: String)
+
+        return df[df[flag_type] .!= "0"]
+end
+
+
+function gems(icd_code:: String;
+        map_to:: String = "icd10",
+        flag_type:: String = "",
+        show_flags:: Bool = false)
+
+    if map_to == "icd10"
+        df = CSV.read("gems9_10.csv")
+    elseif map_to == "icd9"
+        df = CSV.read("gems10_9.csv")
+    end
+
+
+    if length(flag_type) > 0
+
+        df = set_map_type(df,flag_type)
+    end
+
+
+    if show_flags
+
+        my_query = @linq df |>
+            where(:source .== icd_code) |>
+            select(names(df))
+
+    else
+
+        my_query = @linq df |>
+                where(:source .== icd_code) |>
+                select(:source,
+                    :target,
+                    :descriptions)
+    end
+
+    return my_query
+end
